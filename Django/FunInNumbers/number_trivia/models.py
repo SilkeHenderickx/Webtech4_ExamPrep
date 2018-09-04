b3dedb5d5372d77c83fde1f98b6131db7020fe76
SilkeHenderickx <silke.henderickx@student.ap.be>
2018-09-04 11:05:35 +0200

    model instead of class

--- Django/FunInNumbers/number_trivia/models.py
+++ Django/FunInNumbers/number_trivia/models.py
@@ -4,3 +4,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Trivia(models.Model):
+    text = models.CharField(max_length = 200)
+    number = models.IntegerField()
+
+def __str__(self):
+    return self.text
--- Django/FunInNumbers/number_trivia/views.py
+++ Django/FunInNumbers/number_trivia/views.py
@@ -2,19 +2,10 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .models import Trivia
 
 # Create your views here.
 
 def index(request):
+    trivia = Trivia.objects.all()
     return render(request, 'index.html', {'trivia': trivia})
-
-class Trivia:
-    def __init__(self, text, number):
-        self.text = text
-        self.number = number
-
-trivia = [
-Trivia('42 is the number of gallons that one barrel of petroleum holds.', 42),
-Trivia('42 is the number of kilometers in a marathon.', 42),
-Trivia('23 is the number of crosses on Calvary in the Monty Python film Life Of Brian.', 23),
-]
