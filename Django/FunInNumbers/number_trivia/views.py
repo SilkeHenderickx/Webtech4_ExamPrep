100915f2a3bb24bc1937239542fd8dbf18615d65
SilkeHenderickx <silke.henderickx@student.ap.be>
2018-09-04 10:56:02 +0200

    Trivia class instead of context

--- Django/FunInNumbers/number_trivia/views.py
+++ Django/FunInNumbers/number_trivia/views.py
@@ -4,12 +4,17 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    number = 42
-    text = '42 is the number of gallons that one barrel of petroleum holds.'
 
-    context = {
-    'trivia_number': number,
-    'trivia_text': text,
-    }
-    return render(request, 'index.html', context)
+def index(request):
+    return render(request, 'index.html', {'trivia': trivia})
+
+class Trivia:
+    def __init__(self, text, number):
+        self.text = text
+        self.number = number
+
+trivia = [
+Trivia('42 is the number of gallons that one barrel of petroleum holds.', 42),
+Trivia('42 is the number of kilometers in a marathon.', 42),
+Trivia('23 is the number of crosses on Calvary in the Monty Python film Life Of Brian.', 23),
+]
