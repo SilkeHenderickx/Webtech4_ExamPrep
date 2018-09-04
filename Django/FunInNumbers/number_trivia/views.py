767a7b6649a5296e91491707f78ccad9ca71d185
SilkeHenderickx <silke.henderickx@student.ap.be>
2018-09-04 09:16:34 +0200

    view renders index.html instead of base HttpResponse

--- Django/FunInNumbers/number_trivia/views.py
+++ Django/FunInNumbers/number_trivia/views.py
@@ -2,8 +2,7 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Let me blow your socks off with some number trivia!</h1>')
+    return render(request, 'index.html')
