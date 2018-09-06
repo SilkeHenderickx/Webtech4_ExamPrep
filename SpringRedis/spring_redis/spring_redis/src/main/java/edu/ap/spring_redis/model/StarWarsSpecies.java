4c16cbe0f4422f1db048bfbc3af0d3398c3cfa12
SilkeHenderickx <silke.henderickx@student.ap.be>
2018-09-06 11:11:26 +0200

    class done

--- SpringRedis/spring_redis/spring_redis/src/main/java/edu/ap/spring_redis/model/StarWarsSpecies.java
+++ SpringRedis/spring_redis/spring_redis/src/main/java/edu/ap/spring_redis/model/StarWarsSpecies.java
@@ -1,4 +1,40 @@
 package edu.ap.spring_redis.model;
 
 public class StarWarsSpecies {
+
+    private String name;
+    private String classification;
+    private String language;
+
+    public StarWarsSpecies() {    }
+
+    public StarWarsSpecies(String name, String classification, String language) {
+        this.name = name;
+        this.classification = classification;
+        this.language = language;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getClassification() {
+        return classification;
+    }
+
+    public void setClassification(String classification) {
+        this.classification = classification;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
 }
